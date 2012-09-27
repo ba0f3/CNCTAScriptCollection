347b4f5daa41bd7a9ce9cd20993d1fba7cfd6d95
Bruce Doan <rgv151@gmail.com>
2012-09-27 15:59:34 -0400

    clean the code

--- content.js
+++ content.js
@@ -1,13 +1,14 @@
 var USERSCRIPT_PATH = "http://userscripts.org/scripts/source/";
 
 function isStorageSupported() {
-  try {
-    return 'localStorage' in window && window['localStorage'] !== null;
-  } catch (e) {
-    return false;
-  }
+    try {
+        return 'localStorage' in window && window.localStorage !== null;
+    }
+    catch (e) {
+        return false;
+    }
 }
-if(!isStorageSupported()) {
+if (!isStorageSupported()) {
     console.warn("Local Storage is supported!");
 }
 
@@ -18,35 +19,34 @@ function createRemoteScriptElement(url) {
 }
 
 
-function processResponse(settings){
-    CNCTA_SCRIPTS = JSON.parse(settings['CNCTA_SCRIPTS']);
-    CNCTA_REMOTE = settings['CNCTA_REMOTE'];
-    CNCTA_ENABLED = JSON.parse(settings['CNCTA_ENABLED']);
-    CNCTA_GA = settings['CNCTA_GA'] || true;
+function processResponse(settings) {
+    var CNCTA_SCRIPTS = JSON.parse(settings['CNCTA_SCRIPTS']);
+    var CNCTA_REMOTE = settings['CNCTA_REMOTE'];
+    var CNCTA_ENABLED = JSON.parse(settings['CNCTA_ENABLED']);
+    var CNCTA_GA = settings['CNCTA_GA'] || true;
 
-    for(i in CNCTA_SCRIPTS)  {
+    for (var i in CNCTA_SCRIPTS) {
         var script = CNCTA_SCRIPTS[i];
 
-        if(CNCTA_ENABLED[script.id] == true) {
+        if (CNCTA_ENABLED[script.id] === true) {
             var url = '';
-            if(CNCTA_REMOTE == true)
-            {
+            if (CNCTA_REMOTE === true) {
                 url = USERSCRIPT_PATH + script.id + ".user.js";
             }
-            else
-            {
+            else {
                 url = chrome.extension.getURL('/') + "scripts/" + script.id + ".user.js";
             }
             //console.log("Attaching script: " + script.name);
-            createRemoteScriptElement(url);        
-        }    
+            createRemoteScriptElement(url);
+        }
     }
 }
 
 function sendMessage(request, callback) {
-    if(typeof chrome.extension.sendMessage == 'undefined') {
+    if (typeof chrome.extension.sendMessage == 'undefined') {
         chrome.extension.sendRequest(request, callback);
-    } else {
+    }
+    else {
         chrome.extension.sendMessage(request, callback);
     }
 }
@@ -56,5 +56,10 @@ var CNCTA_SCRIPTS = null;
 var CNCTA_REMOTE = null;
 var CNCTA_GA = true;
 
-sendMessage({type: "get", name: ["CNCTA_SCRIPTS", "CNCTA_ENABLED", "CNCTA_REMOTE", "CNCTA_GA"]}, processResponse);
-sendMessage({type: "pageAction"}  , function(response) {});
+sendMessage({
+    type: "get",
+    name: ["CNCTA_SCRIPTS", "CNCTA_ENABLED", "CNCTA_REMOTE", "CNCTA_GA"]
+}, processResponse);
+sendMessage({
+    type: "pageAction"
+}, function(response) {});
--- options.js
+++ options.js
@@ -3,47 +3,51 @@ var CNCTA_ENABLED = JSON.parse(localStorage.getItem('CNCTA_ENABLED'));
 var CNCTA_GA = localStorage.getItem('CNCTA_GA');
 
 var backgroundWindow = chrome.extension.getBackgroundPage();
-$(document).ready(function(){
-	for(var i in CNCTA_SCRIPTS) {
-		var script = CNCTA_SCRIPTS[i];
+$(document).ready(function() {
+    for (var i in CNCTA_SCRIPTS) {
+        var script = CNCTA_SCRIPTS[i];
 
-		var li = $('<div>');
-		var html = "<div class='name'>";
-		html+= "<input id='s-" + script.id + "' class='cb'" +  ((CNCTA_ENABLED[script.id] === true)?" checked='checked'":'') + " name='cb-"+script.id+"' type='checkbox' value='"+script.id+"' >";
-		html+= "<label for='s-" + script.id + "'>" + script.name + "</label>";
-		html+= "<span class='version'>" + script.version + "</span>";
-		html+= "[<a title='Go to homepage' target='_blank' href='http://userscripts.org/scripts/show/" +  script.id+ "'>⚓</a>]";
-		html+= "</div>";
-		li.append(html).appendTo('#scripts');
-	}
+        var li = $('<div>');
+        var html = "<div class='name'>";
+        html += "<input id='s-" + script.id + "' class='cb'" + ((CNCTA_ENABLED[script.id] === true) ? " checked='checked'" : '') + " name='cb-" + script.id + "' type='checkbox' value='" + script.id + "' >";
+        html += "<label for='s-" + script.id + "'>" + script.name + "</label>";
+        html += "<span class='version'>" + script.version + "</span>";
+        html += "[<a title='Go to homepage' target='_blank' href='http://userscripts.org/scripts/show/" + script.id + "'>⚓</a>]";
+        html += "</div>";
+        li.append(html).appendTo('#scripts');
+    }
 
-	if(CNCTA_GA) {
-		$("#ga").attr("checked", "checked");
+    if (CNCTA_GA) {
+        $("#ga").attr("checked", "checked");
 
-		backgroundWindow._gaq.push(['_trackPageview', '/options.html']);
-	}
+        backgroundWindow._gaq.push(['_trackPageview', '/options.html']);
+    }
 
-	$("#close").click(function() {
-		window.close();
-	});
+    $("#close").click(function() {
+        window.close();
+    });
 
-	$("#save").click(function() {
-		var enabled = {};
-		for(var i in CNCTA_SCRIPTS) {
-			var script = CNCTA_SCRIPTS[i];
-			enabled[CNCTA_SCRIPTS[i].id] = $('input[name="cb-' + script.id + '"]').is(':checked');
+    $("#save").click(function() {
+        var enabled = {};
+        for (var i in CNCTA_SCRIPTS) {
+            var script = CNCTA_SCRIPTS[i];
+            enabled[CNCTA_SCRIPTS[i].id] = $('input[name="cb-' + script.id + '"]').is(':checked');
 
-			var tmp = enabled[CNCTA_SCRIPTS[i].id]?'enabled':'disabled';
-			backgroundWindow._gaq.push(['_trackEvent', CNCTA_SCRIPTS[i].name, tmp]);
-		}
-		localStorage.setItem('CNCTA_ENABLED', JSON.stringify(enabled));
+            var tmp = enabled[CNCTA_SCRIPTS[i].id] ? 'enabled' : 'disabled';
+            backgroundWindow._gaq.push(['_trackEvent', CNCTA_SCRIPTS[i].name, tmp]);
+        }
+        localStorage.setItem('CNCTA_ENABLED', JSON.stringify(enabled));
 
-		localStorage.setItem('CNCTA_GA', $("#ga").is(":checked"));
+        localStorage.setItem('CNCTA_GA', $("#ga").is(":checked"));
 
-		$('#message').animate({opacity: 1}, 300, function(){
-			$('#message').animate({opacity: 0}, 3000);
-		});
+        $('#message').animate({
+            opacity: 1
+        }, 300, function() {
+            $('#message').animate({
+                opacity: 0
+            }, 3000);
+        });
 
-		backgroundWindow._gaq.push(['_trackEvent', 'Options', 'saved']);
-	});
+        backgroundWindow._gaq.push(['_trackEvent', 'Options', 'saved']);
+    });
 });
